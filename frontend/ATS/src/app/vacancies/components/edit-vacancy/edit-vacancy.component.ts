38a587dd90aa6ec3c111981a0b601f5f7bb34992
Irina <irina.kbts@gmail.com>
2021-08-09 19:28:38 +0300

    Changing form style

--- frontend/ATS/src/app/vacancies/components/edit-vacancy/edit-vacancy.component.ts
+++ frontend/ATS/src/app/vacancies/components/edit-vacancy/edit-vacancy.component.ts
@@ -5,26 +5,45 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-edit-vacancy',
   templateUrl: './edit-vacancy.component.html',
-  styleUrls: ['./edit-vacancy.component.scss']
+  styleUrls: ['./edit-vacancy.component.scss'],
 })
 export class EditVacancyComponent {
 
   vacancyForm!: FormGroup;
   isOpenCreateStage : Boolean = false;
+  submitted:Boolean = false;
   
   constructor(
     public dialogRef: MatDialogRef<EditVacancyComponent>,
     @Inject(MAT_DIALOG_DATA) public data: {title: string, description:string},
     private fb: FormBuilder) {
-      this.vacancyForm = this.fb.group({
-        name: ['', [Validators.required, Validators.maxLength(20), Validators.minLength(2)]],
-        description: ['']
-      }
-      );
-    }
+    this.vacancyForm = this.fb.group({
+      title: ['', [Validators.required, Validators.maxLength(20), Validators.minLength(2)]],
+      description: [''],
+      requirements: [''],
+      projectId:[''],
+      salaryRang:[''],
+      tierRang:[''],
+      link:[''],
+      isHot:[''],
+      isRemote:[''],
+    },
+    );
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  createVacancy(){
+    console.log(this.vacancyForm);
+    console.log(this.vacancyForm.value);
+    this.submitted = true;
+    // this.dialogRef.close();
+  }
+
+  get vacancyFormControl() {
+    return this.vacancyForm.controls;
   }
 
   displayCreateStage(){
@@ -36,11 +55,11 @@ export class EditVacancyComponent {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes)
+    console.log(changes);
     if(changes.vacancy && this.vacancyForm){
-        this.vacancyForm.get('name')?.setValue(this.data.title)
-        this.vacancyForm.get('description')?.setValue(this.data.description)
+      this.vacancyForm.get('name')?.setValue(this.data.title);
+      this.vacancyForm.get('description')?.setValue(this.data.description);
     }
-}
+  }
 
 }
